9eff261b64354f9cdc8d4cef0da25f9a7d5e5ac1
LeviGibson <72767501+LeviGibson@users.noreply.github.com>
2026-08-04 15:57:20 -0400

    add FaceTurningOctahedronPuzzle.java

---
+++ scrambles/src/main/java/org/worldcubeassociation/tnoodle/puzzle/FaceTurningOctahedronPuzzle.java
@@ -0,0 +1,459 @@
+package org.worldcubeassociation.tnoodle.puzzle;
+
+import org.timepedia.exporter.client.Export;
+import org.worldcubeassociation.tnoodle.scrambles.InvalidScrambleException;
+import org.worldcubeassociation.tnoodle.scrambles.Puzzle;
+import org.worldcubeassociation.tnoodle.scrambles.PuzzleStateAndGenerator;
+import org.worldcubeassociation.tnoodle.svglite.*;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+@Export
+public class FaceTurningOctahedronPuzzle extends Puzzle {
+
+    @Override
+    public String getShortName() {
+        return "fto";
+    }
+
+    @Override
+    public String getLongName() {
+        return "Face Turning Octahedron";
+    }
+
+
+    private static final int STICKER_SIZE = 30;
+    private static final int CENTER_GAP_SIZE = 12;
+    private static final int FACE_GAP_SIZE = 3;
+    private static final int MARGIN = 5;
+
+    private static final String[] MOVE_NAMES = {"R", "L", "U", "D", "F", "B", "BR", "BL", "R'", "L'", "U'", "D'", "F'", "B'", "BR'", "BL'"};
+
+    @Override
+    public Map<String, Color> getDefaultColorScheme() {
+        return new HashMap<>(defaultColorScheme);
+    }
+
+    private static final Map<String, Color> defaultColorScheme = new HashMap<>();
+
+    static {
+        defaultColorScheme.put("B", Color.BLUE);
+        defaultColorScheme.put("D", Color.YELLOW);
+        defaultColorScheme.put("F", Color.GREEN);
+        defaultColorScheme.put("L", new Color(124, 2, 158)); // Purple
+        defaultColorScheme.put("R", Color.RED);
+        defaultColorScheme.put("U", Color.WHITE);
+        defaultColorScheme.put("BL", new Color(255, 128, 0)); // Orange
+        defaultColorScheme.put("BR", Color.GRAY);
+    }
+
+    //Placeholder function
+    //Do not use in production plz :)
+    private String generateRandomMoveScramble(Random r){
+        StringBuilder sb = new StringBuilder();
+        int lastFace = -1;
+
+        for (int i = 0; i < this.getRandomMoveCount(); i++) {
+            if (i > 0) sb.append(" ");
+
+            int face;
+            do {
+                face = r.nextInt(8);
+            } while (lastFace == face);
+
+            int dir = r.nextInt(2) + 1;
+            sb.append(MOVE_NAMES[(dir-1) * 8 + face]);
+
+            lastFace = face;
+        }
+
+        return sb.toString();
+    }
+
+    @Override
+    public PuzzleStateAndGenerator generateRandomMoves(Random r) {
+
+        String scramble = this.generateRandomMoveScramble(r);
+        PuzzleState state;
+        try {
+            state = getSolvedState().applyAlgorithm(scramble);
+        } catch (InvalidScrambleException e) {
+            throw new RuntimeException(e);
+        }
+        return new PuzzleStateAndGenerator(state, scramble);
+    }
+
+    public class FaceTurningOctahedronState extends PuzzleState {
+
+        private final int[][] image = new int[8][9];
+
+        public FaceTurningOctahedronState(){
+            for (int face = 0; face < 8; face++) {
+                for (int sticker = 0; sticker < 9; sticker++) {
+                    image[face][sticker] = face;
+                }
+            }
+        }
+
+        //Copy Constructor
+        public FaceTurningOctahedronState(FaceTurningOctahedronState other){
+            for (int face = 0; face < 8; face++) {
+                System.arraycopy(other.image[face], 0, this.image[face], 0, 9);
+            }
+        }
+
+        @Override
+        public Map<String, FaceTurningOctahedronState> getSuccessorsByName() {
+            Map<String, FaceTurningOctahedronState> successors = new HashMap<>();
+
+            for (int face = 0; face < 8; face++) {
+                for (int dir = 1; dir < 3; dir++) {
+                    String key = MOVE_NAMES[face + 8 * (dir-1)];
+                    FaceTurningOctahedronState successor = new FaceTurningOctahedronState(this);
+                    successor.turn(face, dir);
+                    successors.put(key, successor);
+                }
+            }
+
+            return successors;
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            if (this == other) {
+                return true;
+            }
+
+            if (other == null || getClass() != other.getClass()) {
+                return false;
+            }
+
+            FaceTurningOctahedronState otherFto = (FaceTurningOctahedronState) other;
+            return Arrays.deepEquals(this.image, otherFto.image);
+        }
+
+        @Override
+        public int hashCode() {
+            return Arrays.deepHashCode(image);
+        }
+
+        private double[] rotatePoint(double[] point, double[] center){
+            if (point.length != 2) throw new IllegalArgumentException("Invalid point length");
+            double x = point[0];
+            double y = point[1];
+            double h = center[0];
+            double k =  center[1];
+
+            return new double[]{h - (y - k), k + (x-h)};
+        }
+
+        private double[] translate_right(double[] point){
+            if (point.length != 2) throw new IllegalArgumentException("Invalid point length");
+            return new double[]{point[0] + STICKER_SIZE * 3 + CENTER_GAP_SIZE, point[1]};
+        }
+
+
+        //barycentric interpolation
+        //(a.k.a, a weighted mean of the three points where i and j are the weights)
+        private double[] facePoint(double[] a, double[] b, double[] c, int i, int j) {
+            double bWeight = i / 3.0;
+            double cWeight = j / 3.0;
+            double aWeight = 1 - bWeight - cWeight;
+            return new double[] {
+                aWeight * a[0] + bWeight * b[0] + cWeight * c[0],
+                aWeight * a[1] + bWeight * b[1] + cWeight * c[1]
+            };
+        }
+
+        private Path sticker(double[] a, double[] b, double[] c, Color color) {
+            Path p = new Path();
+            p.setStroke(Color.BLACK);
+            p.setFill(color);
+            p.moveTo(a[0], a[1]);
+            p.lineTo(b[0], b[1]);
+            p.lineTo(c[0], c[1]);
+            p.closePath();
+
+            return p;
+        }
+
+        private void drawFace(Svg svg, Color[] scheme, int[] stickerColors, boolean rightSide, int direction){
+            int m = MARGIN;
+
+            //Compute the three outer points of the face based on
+            //dir and rightSide
+            double[] a = {m + (STICKER_SIZE*1.5), m+(STICKER_SIZE*1.5) - FACE_GAP_SIZE};
+            double[] b = {m, m - FACE_GAP_SIZE};
+            double[] c = {3*STICKER_SIZE + m, m - FACE_GAP_SIZE};
+
+            double[] center = {m + (STICKER_SIZE*1.5), m+(STICKER_SIZE*1.5)};
+
+            //Move the outer points of the face to the right location
+            //in the draw scramble
+            for (int i = 0; i < direction; i++) {
+                a = rotatePoint(a, center);
+                b = rotatePoint(b, center);
+                c = rotatePoint(c, center);
+            }
+            if (rightSide) {
+                a = translate_right(a);
+                b = translate_right(b);
+                c = translate_right(c);
+            }
+
+            //Draw outline of face
+            Path p = new Path();
+            p.moveTo(a[0], a[1]);
+            p.lineTo(b[0], b[1]);
+            p.lineTo(c[0], c[1]);
+            p.closePath();
+
+            p.setFill(Color.WHITE);
+            p.setStroke(Color.BLACK);
+
+            svg.appendChild(p);
+
+            //Draw all the stickers on the face
+            //The location of the stickers are computed dynamically
+            //based on the points a, b, and c from above
+            //
+            //Mess around with the coordinate system here if you want
+            //https://www.desmos.com/calculator/k6k33zuxel
+            int stickerIndex = 0;
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3-i; j++) {
+                    double[] point0;
+                    double[] point1;
+                    double[] point2;
+
+                    //Draw center stickers
+                    if (i + j < 2) {
+                        point0 = facePoint(a, b, c, i+1, j);
+                        point1 = facePoint(a, b, c, i, j+1);
+                        point2 = facePoint(a, b, c, i + 1, j + 1);
+
+                        Color color = scheme[stickerColors[stickerIndex++]];
+
+                        Path sticker = sticker(point0, point1, point2, color);
+                        svg.appendChild(sticker);
+                    }
+
+                    //Draw corner and edge stickers
+                    point0 = facePoint(a, b, c, i, j);
+                    point1 = facePoint(a, b, c, i+1, j);
+                    point2 = facePoint(a, b, c, i, j+1);
+                    Color color = scheme[stickerColors[stickerIndex++]];
+
+                    Path sticker = sticker(point0, point1, point2, color);
+
+                    svg.appendChild(sticker);
+                }
+            }
+        }
+
+        @Override
+        protected Svg drawScramble(Map<String, Color> colorScheme) {
+            Dimension preferredSize = getPreferredSize();
+            Svg svg = new Svg(preferredSize);
+            svg.setStroke(2, 10, "round");
+
+            Color[] scheme = new Color[8];
+            for (int i = 0; i < scheme.length; i++) {
+                scheme[i] = colorScheme.get(MOVE_NAMES[i]);
+            }
+
+            for (int face = 0; face < 8; face++) {
+                drawFace(svg, scheme, image[face],
+                    isFaceRenderedOnRightSide(face),
+                    getFaceRotation(face));
+            }
+
+            return svg;
+        }
+
+        private int getFaceRotation(int face){
+            switch (face) {
+                //R
+                case 0: return 1;
+                //L
+                case 1: return 3;
+                //U
+                case 2: return 0;
+                //D
+                case 3: return 2;
+                //F
+                case 4: return 2;
+                //B
+                case 5: return 0;
+                //BR
+                case 6: return 3;
+                //BL
+                case 7: return 1;
+            }
+
+            throw new IllegalArgumentException("Invalid face");
+        }
+
+        private boolean isFaceRenderedOnRightSide(int face) {
+            switch (face) {
+                //R
+                case 0: return false;
+                //L
+                case 1: return false;
+                //U
+                case 2: return false;
+                //D
+                case 3: return true;
+                //F
+                case 4: return false;
+                //B
+                case 5: return true;
+                //BR
+                case 6: return true;
+                //BL
+                case 7: return true;
+            }
+
+            throw new IllegalArgumentException("Invalid face");
+        }
+
+        private void threeCycle(int f1, int s1, int f2, int s2, int f3, int s3, int[][] image) {
+            int temp = image[f3][s3];
+            image[f3][s3] = image[f2][s2];
+            image[f2][s2] = image[f1][s1];
+            image[f1][s1] = temp;
+        }
+
+        private void turn(int side, int dir) {
+            dir %= 3;
+            for (int i = 0; i < dir; i++) {
+                turn(side);
+            }
+        }
+
+        private void turn(int side) {
+            switch (side) {
+                case 0: // R
+                    threeCycle(0, 0, 0, 5, 0, 2, image);
+                    threeCycle(0, 3, 0, 6, 0, 7, image);
+                    threeCycle(0, 1, 0, 8, 0, 4, image);
+                    threeCycle(4, 6, 2, 3, 6, 7, image);
+                    threeCycle(4, 1, 2, 4, 6, 8, image);
+                    threeCycle(2, 1, 6, 4, 4, 8, image);
+                    threeCycle(2, 0, 6, 2, 4, 5, image);
+                    threeCycle(4, 0, 2, 2, 6, 5, image);
+                    threeCycle(1, 1, 5, 8, 3, 4, image);
+                    break;
+                case 1: // L
+                    threeCycle(1, 3, 1, 6, 1, 7, image);
+                    threeCycle(1, 2, 1, 0, 1, 5, image);
+                    threeCycle(1, 4, 1, 1, 1, 8, image);
+                    threeCycle(2, 6, 4, 3, 7, 7, image);
+                    threeCycle(2, 8, 4, 1, 7, 4, image);
+                    threeCycle(2, 1, 4, 4, 7, 8, image);
+                    threeCycle(0, 1, 3, 8, 5, 4, image);
+                    threeCycle(2, 5, 4, 0, 7, 2, image);
+                    threeCycle(2, 0, 4, 2, 7, 5, image);
+                    break;
+                case 2: // U
+                    threeCycle(2, 0, 2, 5, 2, 2, image);
+                    threeCycle(2, 6, 2, 7, 2, 3, image);
+                    threeCycle(2, 1, 2, 8, 2, 4, image);
+                    threeCycle(4, 1, 7, 8, 6, 4, image);
+                    threeCycle(0, 1, 1, 4, 5, 8, image);
+                    threeCycle(0, 8, 1, 1, 5, 4, image);
+                    threeCycle(0, 6, 1, 3, 5, 7, image);
+                    threeCycle(0, 0, 1, 2, 5, 5, image);
+                    threeCycle(0, 5, 1, 0, 5, 2, image);
+                    break;
+                case 3: // D
+                    threeCycle(3, 0, 3, 5, 3, 2, image);
+                    threeCycle(3, 3, 3, 6, 3, 7, image);
+                    threeCycle(3, 1, 3, 8, 3, 4, image);
+                    threeCycle(6, 6, 7, 3, 4, 7, image);
+                    threeCycle(6, 0, 7, 2, 4, 5, image);
+                    threeCycle(6, 1, 7, 4, 4, 8, image);
+                    threeCycle(6, 5, 7, 0, 4, 2, image);
+                    threeCycle(6, 8, 7, 1, 4, 4, image);
+                    threeCycle(0, 4, 5, 1, 1, 8, image);
+                    break;
+                case 4: // F
+                    threeCycle(4, 2, 4, 0, 4, 5, image);
+                    threeCycle(4, 3, 4, 6, 4, 7, image);
+                    threeCycle(4, 1, 4, 8, 4, 4, image);
+                    threeCycle(1, 6, 0, 3, 3, 7, image);
+                    threeCycle(1, 0, 0, 2, 3, 5, image);
+                    threeCycle(1, 1, 0, 4, 3, 8, image);
+                    threeCycle(1, 5, 0, 0, 3, 2, image);
+                    threeCycle(1, 8, 0, 1, 3, 4, image);
+                    threeCycle(2, 1, 6, 8, 7, 4, image);
+                    break;
+                case 5: // B
+                    threeCycle(5, 0, 5, 5, 5, 2, image);
+                    threeCycle(5, 6, 5, 7, 5, 3, image);
+                    threeCycle(5, 1, 5, 8, 5, 4, image);
+                    threeCycle(7, 6, 6, 3, 2, 7, image);
+                    threeCycle(7, 5, 6, 0, 2, 2, image);
+                    threeCycle(7, 8, 6, 1, 2, 4, image);
+                    threeCycle(7, 0, 6, 2, 2, 5, image);
+                    threeCycle(7, 1, 6, 4, 2, 8, image);
+                    threeCycle(3, 1, 0, 8, 1, 4, image);
+                    break;
+                case 6: // BR
+                    threeCycle(6, 2, 6, 0, 6, 5, image);
+                    threeCycle(6, 3, 6, 6, 6, 7, image);
+                    threeCycle(6, 4, 6, 1, 6, 8, image);
+                    threeCycle(5, 6, 3, 3, 0, 7, image);
+                    threeCycle(5, 5, 3, 0, 0, 2, image);
+                    threeCycle(5, 8, 3, 1, 0, 4, image);
+                    threeCycle(0, 5, 5, 0, 3, 2, image);
+                    threeCycle(0, 8, 5, 1, 3, 4, image);
+                    threeCycle(4, 8, 2, 4, 7, 1, image);
+                    break;
+                case 7: // BL
+                    threeCycle(7, 0, 7, 5, 7, 2, image);
+                    threeCycle(7, 6, 7, 7, 7, 3, image);
+                    threeCycle(7, 1, 7, 8, 7, 4, image);
+                    threeCycle(3, 6, 5, 3, 1, 7, image);
+                    threeCycle(3, 5, 5, 0, 1, 2, image);
+                    threeCycle(3, 8, 5, 1, 1, 4, image);
+                    threeCycle(3, 0, 5, 2, 1, 5, image);
+                    threeCycle(3, 1, 5, 4, 1, 8, image);
+                    threeCycle(6, 1, 2, 8, 4, 4, image);
+                    break;
+                default:
+                    assert false;
+            }
+        }
+    }
+
+    private int getPreferredWidth(){
+        return (3 * 2 * STICKER_SIZE) + (CENTER_GAP_SIZE) + (2*MARGIN);
+    }
+
+    private int getPreferredHeight(){
+        return( 3 * STICKER_SIZE) + (2 * MARGIN);
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(getPreferredWidth(),
+            getPreferredHeight());
+    }
+
+    @Override
+    public PuzzleState getSolvedState() {
+        return new FaceTurningOctahedronState();
+    }
+
+    @Override
+    protected int getRandomMoveCount() {
+        //34 moves is enough to produce a decently random scramble
+        //not used in production for obvious reasons
+        return 34;
+    }
+}
+
